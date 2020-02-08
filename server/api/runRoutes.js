edd4a00f4c526af740bca9b0f2a6ded604bbaec4
Jake Rothenberg <jrothenberg125@gmail.com>
2020-02-07 20:08:37 -0500

    added two new routes

---
+++ server/api/runRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const {Route} = require('../db/models')
+
+//eventually will want to add query params here to get completed and non-complete run routes
+router.get('/', async (req, res, next) => {
+    try {
+        const allRunRoutes = await Route.getAllRoutes(req.user.id)
+        res.status(200).send(allRunRoutes)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/:runId', async (req, res, next) => {
+    try {
+        const run = await Route.findByPK(req.params.runId)
+        res.status(200).send(run)
+    } catch (error) {
+        
+    }
+})
+
+module.exports = router
