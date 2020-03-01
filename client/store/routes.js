bd446cbe0f6ef266338e3c665e003b27767ab939
Jake Rothenberg <jrothenberg125@gmail.com>
2020-03-01 18:03:03 -0500

    added create new route thunk

--- client/store/routes.js
+++ client/store/routes.js
@@ -1,3 +1,5 @@
+import Axios from "axios"
+
 const CREATE_NEW_ROUTE = "CREATE_NEW_ROUTE"
 
 const createNewRoute = (route) => {
@@ -7,8 +9,18 @@ const createNewRoute = (route) => {
     }
 }
 
+export const createNewRouteThunk = (coords, state, city, country) => {
+    return async (dispatch) => {
+        try {
+            const newRoute = Axios.post({coords, state, city, country})
+            dispatch(createNewRoute(newRoute))
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
 
-const routeReducer = (state = {}, action) => {
+export default routeReducer = (state = {}, action) => {
     switch (action.type) {
         case CREATE_NEW_ROUTE:
             return {...state, routes: [...state.routes, action.route]}
